2b92dd0362b76f23bc4b50b5c54f2b3349e2df23
Miika Valkonen <miika.valkonen08@gmail.com>
2026-05-05 12:37:05 +0300

    15. Patientor backend, step7

--- patientor/backend/src/routes/patients.ts
+++ patientor/backend/src/routes/patients.ts
@@ -1,27 +1,36 @@
-import express, { type Response} from 'express';
-import type { NonSensitivePatients } from '../types/diagnosis.ts';
+import express, { type Request, type Response, type NextFunction } from 'express';
+import { newEntrySchema, type NewPatientEntry, type NonSensitivePatients, type PatientEntry } from '../types/diagnosis.ts';
 import patientsService from '../services/patientsService.ts';
-import parseNewPatientEntry from '../utils.ts';
-
+import { z } from 'zod';
 
 const router = express.Router();
+
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
+  try {
+    newEntrySchema.parse(req.body);
+    next();
+  } catch (error: unknown) {
+    next(error);
+  }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
 
 router.get('/', (_req, res: Response<NonSensitivePatients[]>) => {
   res.send(patientsService.getNonSensitivePatients());
 });
 
-router.post('/', (req, res) => {
-  try {
-    const newPatientEntry = parseNewPatientEntry(req.body);
-    const addedEntry = patientsService.addPatient(newPatientEntry);
-    res.json(addedEntry);
-  } catch (error: unknown) {
-    let errorMessage = 'Something went wrong.';
-    if (error instanceof Error) {
-      errorMessage += ' Error: ' + error.message;
-    }
-    res.status(400).send(errorMessage);
-  }
+router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatientEntry>, res: Response<PatientEntry>) => {
+  const addedEntry = patientsService.addPatient(req.body);
+  res.json(addedEntry);
 });
+
+router.use(errorMiddleware);
 
 export default router;
--- patientor/backend/src/types/diagnosis.ts
+++ patientor/backend/src/types/diagnosis.ts
@@ -1,3 +1,20 @@
+import { z } from 'zod';
+
+export const Gender = {
+  Male: 'male',
+  Female: 'female',
+  Other: 'other',
+} as const;
+
+export const newEntrySchema = z.object({
+  name: z.string(),
+  dateOfBirth: z.iso.date(),
+  ssn: z.string(),
+  gender: z.enum(Gender),
+  occupation: z.string(),
+});
+
+
 export interface Diagnosis {
   code: string;
   name: string;
@@ -9,18 +26,16 @@ export interface Patient {
   name: string;
   dateOfBirth: string;
   ssn: string;
-  gender: string;
+  gender: Gender;
   occupation: string;
 }
 
 export type NonSensitivePatients = Omit<Patient, 'ssn'>;
 
-export type NewPatientEntry = Omit<Patient, 'id'>;
+export type NewPatientEntry = z.infer<typeof newEntrySchema>;
 
-export const Gender = {
-  Male: 'male',
-  Female: 'female',
-  Other: 'other',
-} as const;
+export interface PatientEntry extends NewPatientEntry {
+  id: string;
+}
 
 export type Gender = typeof Gender[keyof typeof Gender];
