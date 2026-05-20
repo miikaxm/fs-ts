a86cf187ae83a0c902956076141b2141dd7da3e4
Miika Valkonen <miika.valkonen08@gmail.com>
2026-05-20 12:21:46 +0300

    22. Flight diaries, step4

--- flightdiaries/frontend/src/App.tsx
+++ flightdiaries/frontend/src/App.tsx
@@ -61,24 +61,49 @@ const App = () => {
       <h1>Add new diary</h1>
       <p id="error" style={{ color: 'red' }}>{error}</p>
       <form onSubmit={diaryCreation}>
+
         {/* Date select */}
-        <label htmlFor="date">Date:</label><br />
-        <input type="text" id="date" name="date" value={newDate} onChange={(event) => setNewDate(event.target.value)} /><br />
+        <label htmlFor="date"><b>Date:</b></label><br />
+        <input type="date" id="date" name="date" value={newDate} onChange={(event) => setNewDate(event.target.value)} /><br />
 
         {/* Weather input */}
-        <label htmlFor="weather">Weather:</label><br />
-        <input type="text" id="weather" name="weather" value={newWeather} onChange={(event) => setNewWeather(event.target.value)} /><br />
+        <h4>Weather:</h4>
+        <label htmlFor="sunny">Sunny:</label>
+        <input type="radio" id="sunny" name="weather" value={'sunny'} onChange={(event) => setNewWeather(event.target.value)} /><br />
+        
+        <label htmlFor="rainy">Rainy: </label>
+        <input type="radio" id="rainy" name="weather" value={'rainy'} onChange={(event) => setNewWeather(event.target.value)} /><br />
+
+        <label htmlFor="cloudy">Cloudy:</label>
+        <input type="radio" id="cloudy" name="weather" value={'cloudy'} onChange={(event) => setNewWeather(event.target.value)} /><br />
+
+        <label htmlFor="stormy">Stormy</label>
+        <input type="radio" id="stormy" name="weather" value={'stormy'} onChange={(event) => setNewWeather(event.target.value)} /><br />
+
+        <label htmlFor="windy">Windy:</label>
+        <input type="radio" id="windy" name="weather" value={'windy'} onChange={(event) => setNewWeather(event.target.value)} /><br /><br />
 
         {/* Visiblity input */}
-        <label htmlFor="visibility">Visibility:</label><br />
-        <input type="text" id="visibility" name="visibility" value={newVisibility} onChange={(event) => setNewVisibility(event.target.value)}/><br />
+        <h4>Visiblity:</h4>
+
+        <label htmlFor="great">Great:</label>
+        <input type="radio" id="great" name="visibility" value={'great'} onChange={(event) => setNewVisibility(event.target.value)}/><br />
+
+        <label htmlFor="good">Good:</label>
+        <input type="radio" id="good" name="visibility" value={'good'} onChange={(event) => setNewVisibility(event.target.value)}/><br />
+
+        <label htmlFor="ok">Ok:</label>
+        <input type="radio" id="ok" name="visibility" value={'ok'} onChange={(event) => setNewVisibility(event.target.value)}/><br />
+
+        <label htmlFor="poor">Poor:</label>
+        <input type="radio" id="poor" name="visibility" value={'poor'} onChange={(event) => setNewVisibility(event.target.value)}/><br /><br />
 
         {/* Comment field */}
-        <label htmlFor="comment">Comment:</label><br />
+        <label htmlFor="comment"><b>Comment:</b></label><br />
         <textarea name="comment" id="comment" rows={4} cols={40} value={newComment} onChange={(event) => setNewComment(event.target.value)}></textarea><br />
 
-        {/* Input button */}
-        <input type="submit" />
+        {/* Submit button */}
+        <input type="submit"/>
       </form>
       <div>
         <h1>Recent Diaries</h1>
